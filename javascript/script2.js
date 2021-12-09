6891c7cf8c9eb3c1604ff5deb500213e73597a64
LucasVelasquez <87956384+LucasVelasquez@users.noreply.github.com>
2021-12-08 23:53:56 -0300

    Add files via upload

---
+++ javascript/script2.js
@@ -0,0 +1,114 @@
+
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quién fue el último en integrarse al grupo?",
+    answer: "Rachel",
+    options: [
+      "Phoebe",
+      "Rachel",
+      "Joey",
+      "Chandler"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Chandler se encerro en una caja para:",
+    answer: "Demostrarle su amistad a Joey",
+    options: [
+      "Dejar de fumar",
+      "Hacerle una broma a Ross",
+      "Demostrarle su amistad a Joey",
+      "Para que no lo encuentra Janice"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Joey compra una enciclopedia de:",
+    answer: "La letra V",
+    options: [
+      "La letra V",
+      "La letra G",
+      "Historia",
+      "Geografía"
+    ]
+  },
+    {
+    numb: 4,
+    question: "En un capítulo encuentran unas esposas eróticas, de quién eran?",
+    answer: "De la abuela de Monica y Ross",
+    options: [
+      "De Phoebe",
+      "De la abuela de Monica y Ross",
+      "De Gunther",
+      "De los padres de Monica y Ross"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Quienes se casaron en la puerta del Central Perk?",
+    answer: "Phoebe y Mike",
+    options: [
+      "Phoebe y Mike",
+      "Monica y Chandler",
+      "Gunther y su pareja",
+      "Ross, obviamente"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Cómo se llaman los cuadros de Phoebe?",
+    answer: "Gladys y Glynnis",
+    options: [
+      "Gladys y Rebecca",
+      "Rebecca y Glynnis",
+      "Elizabeth y Rebecca",
+      "Gladys y Glynnis"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Cómo consiguieron el gran mueble Chandler y Joey?",
+    answer: "Lo construyo Joey",
+    options: [
+      "Lo cambiaron por una canoa",
+      "Lo heredaron de la familia de Joey",
+      "Lo construyo Joey",
+      "Era del vecino que falleció"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Cuántas hermanas tiene Joey?",
+    answer: "7 hermanas",
+    options: [
+      "9 hermanas",
+      "7 hermanas",
+      "5 hermanas",
+      "11 hermanas"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Ross se sorprendió de Bonnie cuando:",
+    answer: "La vió calva",
+    options: [
+      "Se dió cuenta que tenía una pierna ortopédica",
+      "La vió calva",
+      "La encontró besandose con Joey",
+      "Supo que estaba divorciada"
+    ]
+  },
+    {
+    numb: 10,
+    question: "De qué trabajaba Chandler que no le gustaba?",
+    answer: "Análisis estadístico y reconfiguración de datos",
+    options: [
+      "Análisis estadístico y modificación de datos avanzados",
+      "Ingreso de datos estadísticos y evaluación del comportamiento del avance",
+      "Análisis estadístico y reconfiguración de datos",
+      "Seguimos sin saberlo..."
+    ]
+  },
+];
